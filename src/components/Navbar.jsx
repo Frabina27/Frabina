111f579f4d04a56a1677534f561cd94947f3c194
Snowie <frabina2016@gmail.com>
2026-03-01 09:17:46 -0600

    removed home and made brand color different

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,12 +7,12 @@ export default function Navbar() {
       <div className="nav-inner">
         {/* Brand with heart */}
         <div className="brand">
-          <span className="heart">♡</span> Frabina
-        </div>
+<Link to="/" className="brand">
+  <span className="heart">♡</span> Frabina
+</Link>        </div>
 
         {/* Right-aligned nav links */}
         <div className="nav-links">
-          <Link className="nav-link" to="/">Home</Link>
           <Link className="nav-link" to="/projects">Projects</Link>
           <Link className="nav-link" to="/designs">Designs</Link>
           <Link className="nav-link" to="/resume">Resume</Link>
